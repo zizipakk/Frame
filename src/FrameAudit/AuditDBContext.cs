6575559b2dc396a942c36e79e18881dee9042873
toroktamas <toroktamas@WS-L0015547>
2017-04-03 18:28:03 +0200

    Audit finally

--- src/FrameAudit/AuditDBContext.cs
+++ src/FrameAudit/AuditDBContext.cs
@@ -1,206 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System.Collections;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http;
 using System.Threading;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
-using System.Reflection;
 
 namespace FrameAudit
 {
-    /// <summary>
-    /// This for elastic usage
-    /// BUT DEPENDENT ON EF.Core 1.1
-    /// </summary>
-    public interface IChangeTracker : IInfrastructure<IStateManager>
+    public class AuditDBContext : DbContext
     {
-        bool AutoDetectChangesEnabled { get; set; }
-        IDbContext Context { get; }
-        QueryTrackingBehavior QueryTrackingBehavior { get; set; }
-        void AcceptAllChanges();
-        void DetectChanges();
-        IEnumerable<EntityEntry> Entries();
-        IEnumerable<EntityEntry<TEntity>> Entries<TEntity>() where TEntity : class;
-        bool HasChanges();
-        void TrackGraph(object rootEntity, Action<EntityEntryGraphNode> callback);
-    }
-
-    /// <summary>
-    /// This for elastic usage
-    /// BUT DEPENDENT ON EF.Core 1.1
-    /// </summary>
-    public interface IDbContext : IDisposable, IInfrastructure<IServiceProvider>
-    {
-        ChangeTracker ChangeTracker { get; }
-        DatabaseFacade Database { get; }
-        IModel Model { get; }
-
-        EntityEntry Add(object entity);
-        EntityEntry<TEntity> Add<TEntity>(TEntity entity) where TEntity : class;
-        Task<EntityEntry> AddAsync(object entity, CancellationToken cancellationToken = default(CancellationToken));
-        Task<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default(CancellationToken)) where TEntity : class;
-        void AddRange(IEnumerable<object> entities);
-        void AddRange(params object[] entities);
-        Task AddRangeAsync(params object[] entities);
-        Task AddRangeAsync(IEnumerable<object> entities, CancellationToken cancellationToken = default(CancellationToken));
-        EntityEntry Attach(object entity);
-        EntityEntry<TEntity> Attach<TEntity>(TEntity entity) where TEntity : class;
-        void AttachRange(IEnumerable<object> entities);
-        void AttachRange(params object[] entities);
-        void Dispose();
-        EntityEntry Entry(object entity);
-        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
-        object Find(Type entityType, params object[] keyValues);
-        TEntity Find<TEntity>(params object[] keyValues) where TEntity : class;
-        Task<object> FindAsync(Type entityType, params object[] keyValues);
-        Task<object> FindAsync(Type entityType, object[] keyValues, CancellationToken cancellationToken);
-        Task<TEntity> FindAsync<TEntity>(params object[] keyValues) where TEntity : class;
-        Task<TEntity> FindAsync<TEntity>(object[] keyValues, CancellationToken cancellationToken) where TEntity : class;
-        EntityEntry Remove(object entity);
-        EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class;
-        void RemoveRange(IEnumerable<object> entities);
-        void RemoveRange(params object[] entities);        
-        int SaveChanges();
-        int SaveChanges(bool acceptAllChangesOnSuccess);
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
-        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
-        DbSet<TEntity> Set<TEntity>() where TEntity : class;
-        EntityEntry Update(object entity);
-        EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
-        void UpdateRange(params object[] entities);
-        void UpdateRange(IEnumerable<object> entities);
-        void OnConfiguring(DbContextOptionsBuilder optionsBuilder);
-        void OnModelCreating(ModelBuilder modelBuilder);
-    }
-
-    public partial class AuditDBContext : DbContext
-    {
-        private readonly IEnumerable<EntityState> loggedStates;
-        private readonly IEnumerable<Tuple<EntityEntry, EntityEntry>> loggedEntries;
-        private readonly DbContext context;
+        private string action = ""; //TODO thread safe?
+        private readonly ICommonAudits common;
 
         public AuditDBContext(
-            DbContextOptions<AuditDBContext> options,
+            DbContextOptions options,
+            IHttpContextAccessor context,
             IEnumerable<EntityState> loggedStates,
-            IEnumerable<Tuple<EntityEntry, EntityEntry>> loggedEntityTypes
-            ) : base(options)
+            IEnumerable<Tuple<EntityEntry, EntityEntry>> loggedEntries
+        ) : base(options)
         {
-            this.loggedStates = loggedStates;
-            this.loggedEntries = loggedEntityTypes;
-
-            if (GetType().GetTypeInfo().IsSubclassOf(typeof(DbContext)))
-            {
-                context = ; //.GetProperty("ChangeTracker") ?? new DbContext(null);
-            }
-            else
-            {
-                context = new DbContext(options);
-            }
+            this.common = new CommonAudits(context, loggedStates, loggedEntries); //TODO: Zero DI conf
         }
 
         /// <summary>
-        /// This is for common auditsy
+        /// This is for common audits
         /// </summary>
-        internal virtual DbSet<AuditLog> AuditLogs { get; set; }
+        public virtual DbSet<AuditLog> AuditLogs { get; set; }
 
         /// <summary>
-        /// Cleaning changetracker if exception
+        /// Proxy to get caller
         /// </summary>
-        public virtual void RevertChanges()
-        {
-            foreach (var entry in context.ChangeTracker.Entries())
-            {
-                switch (entry.State)
-                {
-                    case EntityState.Modified:
-                        entry.CurrentValues.SetValues(entry.OriginalValues);
-                        entry.State = EntityState.Unchanged;
-                        break;
-                    case EntityState.Deleted:
-                        entry.State = EntityState.Unchanged;
-                        break;
-                    case EntityState.Added:
-                        entry.State = EntityState.Detached;
-                        break;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Save auditlogs  and logs in one step
-        /// </summary>
-        /// <param name="executiveID"></param>
+        /// <param name="action"></param>
         /// <returns></returns>
-        public async virtual Task<int> SaveChangesAsync(Guid? executiveID, string location = null)
+        public Task<int> SaveChangesAsync([CallerMemberName]string action = "")
         {
-            var result = 0;
+            this.action = action;
+            return SaveChangesAsync();
+        }
 
-            try
-            {
-                // freez tracker like bulk insert
-                context.ChangeTracker.AutoDetectChangesEnabled = false;
+        /// <summary>
+        /// Override async save
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            common.Logger(action, this as DbContext);
+            return base.SaveChangesAsync();
+        }
 
-                //Create changed entries list with logtypes if exists
-                var entries = context.ChangeTracker
-                                .Entries()
-                                .Where(w => loggedStates.Contains(w.State))
-                                .SelectMany(
-                                    e => loggedEntries.Where(w => w.Item1 == e),
-                                    (e, l) => new { entry = e, shadowEntry = l.Item2 })
-                                .OrderBy(o => o.shadowEntry.Entity?.GetType()?.Name);
+        /// <summary>
+        /// Proxy to get caller
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public int SaveChanges([CallerMemberName]string action = "")
+        {
+            this.action = action;
+            return SaveChanges();
+        }
 
-                if (entries.Any())
-                {
-                    var newAudits = new List<AuditLog>();
-                    newAudits.AddRange(
-                                    entries
-                                        .Select(s => new AuditLog(executiveID, s.entry.Entity.GetType().Name, location)
-                                        {
-                                            State = s.entry.State.ToString(),
-                                            Action = "auto", // caller from stack
-                                            Location = ""
-                                        })
-                                );
-                    await context.AddRangeAsync(newAudits); //TODO one step
-
-                    entries
-                        .Where(w => w.shadowEntry != null)
-                        .Select(s => s.shadowEntry)
-                        .Distinct()
-                        .ToList()
-                        .ForEach(shadowEntryFilter =>
-                        {
-                        var entityType = shadowEntryFilter.Entity.GetType();
-                        var newShadowEntities = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(entityType));
-                        entries
-                            .Where(w => w.shadowEntry == shadowEntryFilter)
-                            .Select(s => s.shadowEntry)
-                            .ToList()
-                            .ForEach(shadowEntry =>
-                            {
-                                newShadowEntities.Add(shadowEntry.Entity);
-                            });
-
-                            context.AddRangeAsync(newShadowEntities);
-                        });
-                }
-
-                result = await context.SaveChangesAsync();
-                // back to normal operation
-                context.ChangeTracker.AutoDetectChangesEnabled = true;
-            }
-            catch (Exception e)
-            {
-                RevertChanges();
-                throw new Exception(e.Message);
-            }
-
-            return result;
+        /// <summary>
+        /// Override sync save
+        /// </summary>
+        /// <returns></returns>
+        public override int SaveChanges()
+        {
+            common.Logger(action, this as DbContext);
+            return base.SaveChanges();
         }
     }
 }
---
+++ src/FrameAudit/CommonAudits.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace FrameAudit
+{
+    public interface ICommonAudits
+    {
+        void RevertChanges(IEnumerable<EntityEntry> allEntries);
+
+        void Logger(string action, DbContext dbContext);
+    }
+
+    public class CommonAudits : ICommonAudits
+    {
+        private readonly IHttpContextAccessor context;
+        private readonly IEnumerable<EntityState> loggedStates;
+        private readonly IEnumerable<Tuple<EntityEntry, EntityEntry>> loggedEntries;
+
+        public CommonAudits(
+            IHttpContextAccessor context,
+            IEnumerable<EntityState> loggedStates,
+            IEnumerable<Tuple<EntityEntry, EntityEntry>> loggedEntries
+        )
+        {
+            this.context = context;
+            this.loggedStates = loggedStates.Distinct();
+            this.loggedEntries = loggedEntries.Distinct();
+        }
+
+        /// <summary>
+        /// Restore changetracker if exception
+        /// </summary>
+        public void RevertChanges(IEnumerable<EntityEntry> allEntries)
+        {
+            allEntries.ToList().ForEach(entry =>
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.CurrentValues.SetValues(entry.OriginalValues);
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Save sync auditlogs and shadow logs in one step
+        /// </summary>
+        /// <returns></returns>
+        public void Logger(string action, DbContext dbContext)
+        {
+            var allChangedEntries = dbContext.ChangeTracker.Entries();
+
+            if (allChangedEntries.Any() && loggedStates.Any() && loggedEntries.Any())
+            {
+
+                try
+                {
+                    var filteredChangedEntries = allChangedEntries
+                        .Where(w => loggedStates.Contains(w.State) && loggedEntries.Select(s => s.Item1).Contains(w));
+
+                    if (filteredChangedEntries.Any())
+                    {
+                        // freez tracker like bulk insert
+                        dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
+
+                        var id = context.HttpContext?.User?.Identity;
+                        // By sing necessary put userID into claims
+                        var userId = (id as ClaimsIdentityOptions)?.UserIdClaimType ?? id?.Name;
+                        var location = context.HttpContext?.Request?.Host.Host;
+
+                        // audit
+                        dbContext.AddRange(
+                            filteredChangedEntries
+                            .Select(s =>
+                                new AuditLog(userId, s.Entity.GetType().Name, location)
+                                {
+                                    State = s.State.ToString(),
+                                    Action = action
+                                }));
+
+                        // shadow
+                        dbContext.AddRange(
+                            filteredChangedEntries
+                            .SelectMany(e =>
+                                loggedEntries.Where(w => w.Item1 == e && w.Item2 != null),
+                                (e, s) => new { s.Item1 }));
+
+                        // back to normal operation
+                        dbContext.ChangeTracker.AutoDetectChangesEnabled = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    RevertChanges(allChangedEntries);
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+    }
+}
