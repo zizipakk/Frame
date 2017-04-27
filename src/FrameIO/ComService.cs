8c7d92a3e262188e2e54576b88c921cb5b58701d
toroktamas <tamas.torok@comicsys.hu>
2017-04-27 17:43:49 +0200

    ComService first version

--- src/FrameIO/ComService.cs
+++ src/FrameIO/ComService.cs
@@ -6,16 +6,46 @@ using RJCP.IO.Ports;
 namespace FrameIO
 {
     // Singleton thread safe serial service
-    public static class ComService
+    public sealed class ComService : IDisposable
     {
-        public static SerialPortStream src;
+        public SerialPortStream src = null;
 
-        static ComService()
+        ComService()
         {
-            var c_SourcePort = SerialConfiguration.SourcePort;
-            src = new SerialPortStream(c_SourcePort, 115200, 8, Parity.None, StopBits.One);
+            InitPort();
         }
 
+        public void OpenPort()
+        { }
 
+        public void ClosePort()
+        { }
+
+        public void WritePort()
+        { }
+
+        public void ReadPort()
+        { }
+
+        private void InitPort()
+        {
+            if (src == null)
+            {
+                var c_SourcePort = SerialConfiguration.SourcePort;
+                src = new SerialPortStream(c_SourcePort, 115200, 8, Parity.None, StopBits.One);
+            }
+        }
+
+        private void DisposePort()
+        {
+            if (src != null)
+                src.Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (src?.IsDisposed == false)
+                DisposePort();
+        }
     }
 }
